2137aff9f87df0105dcbad4aaf6a5f61372ef01a
THANATATH <thanatuth.euro@gmail.com>
2022-02-12 22:49:57 +0700

    fix wav extension wrong

--- inputfile.py
+++ inputfile.py
@@ -2,9 +2,9 @@ import os
 from pathlib import Path
 
 
-FILE_INPUT = "input.mp3"
+FILE_INPUT = "2022-01-20 10-19-44.wav"
 FILE_LOCATION = os.path.abspath(FILE_INPUT)
 FILE_DIR = str(Path(FILE_LOCATION).parent)
-audio_wav = Path(str(FILE_DIR+'/'+FILE_INPUT).split(".mp3")[0]+".wav")
+audio_wav = Path(str(FILE_DIR+'/'+FILE_INPUT).split(".mp3")[0])
 
 print("File input: ",FILE_INPUT)
