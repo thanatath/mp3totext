394fd780e5a2e924a005d2e273561bf2797e7bf8
THANATATH <thanatuth.euro@gmail.com>
2022-02-17 16:06:03 +0700

    add while true  check filename

--- inputfile.py
+++ inputfile.py
@@ -1,9 +1,18 @@
 import os
 from pathlib import Path
 
-print("Wav to text")
+FILE_INPUT = 'Null.wav'
 
-FILE_INPUT = input("Input your wave file name (ex. input.wav) :")
+
+while True:
+    file_name = input("Input your wave file name (ex. input.wav) : ")
+    file_name = os.path.abspath(file_name)
+    if os.path.isfile(file_name):
+        FILE_INPUT = file_name
+        break
+    else:
+        print(os.path.abspath(file_name)," File not found !")
+
 FILE_LOCATION = os.path.abspath(FILE_INPUT)
 FILE_DIR = str(Path(FILE_LOCATION).parent)
 audio_wav = Path(str(FILE_DIR+'/'+FILE_INPUT).split(".mp3")[0])
