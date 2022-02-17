65efe21a0b2ad04b7037466aae899f40fcb3a9c0
THANATATH <thanatuth.euro@gmail.com>
2022-02-17 16:15:37 +0700

    fix error path file

--- inputfile.py
+++ inputfile.py
@@ -15,6 +15,6 @@ while True:
 
 FILE_LOCATION = os.path.abspath(FILE_INPUT)
 FILE_DIR = str(Path(FILE_LOCATION).parent)
-audio_wav = Path(str(FILE_DIR+'/'+FILE_INPUT).split(".mp3")[0])
+audio_wav = Path(str(FILE_INPUT).split(".mp3")[0])
 
 print("File input: ",FILE_INPUT)
